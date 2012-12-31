5d2e53152a013bc0712e92236d8bff26c033c3ba
Alistair Thomas <alfie_thomas@hotmail.com>
2012-12-31 13:24:32 +0000

    Update docs styling

--- commit.sh
+++ commit.sh
@@ -5,6 +5,8 @@ exit
 fi	
 
 java -jar build/yuicompressor-2.4.7.jar scripts/jquery.datafilters.js -o scripts/jquery.datafilters.min.js
+echo "Created min version"
+
 git add -A
 git commit -m "$1"
 git push -u origin master
