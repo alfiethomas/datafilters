1edeb317a843f09db0d0e14aafbb8da78f2e87f0
Alistair Thomas <alfie_thomas@hotmail.com>
2013-02-05 21:26:41 +0000

    quick

--- temp.js
+++
@@ -1,5 +0,0 @@
-console.log(window.location.hash);
-window.location.hash = "testClass10=range_£50-£75";
-console.log(window.location.hash);
-var hash = window.location.hash.replace(new RegExp("%A3", 'g'),"£")
-console.log(decodeURIComponent(hash));
